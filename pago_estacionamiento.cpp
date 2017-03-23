f173a14da1cd3128b2a047121df8872aeb3e3e09
carlos pizano <carlos pizano>
2017-03-23 12:12:10 -0600

    modifique el codigo c++(terminado)

--- pago_estacionamiento.cpp
+++ pago_estacionamiento.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-
 /*
 Funcion que calcula la cantidad a pagar por concepto de estacionamiento
     de 1 a 5 horas, la hora cuesta $10
@@ -24,7 +23,19 @@ void casos_de_prueba(){
 
     cout << "TEST CASES" << endl;
     // Agrega tus casos de prueba
-
+	cout <<"Test Data   / Expected result / Actual Result" <<endl;
+	cout <<"    3     /     30              / "<<pago_estacionamiento(3) <<endl;
+	cout <<"    -2    /     0              /  "<<pago_estacionamiento(-2) <<endl;
+	cout <<"    6     /  	54              / "<<pago_estacionamiento(6) <<endl;
+	cout <<"    12    / 	84              / "<<pago_estacionamiento(12) <<endl;
+	cout <<"    0     / 	0              /  "<<pago_estacionamiento(0) <<endl;
+	cout <<"    1     / 	10              / "<<pago_estacionamiento(1) <<endl;
+	cout <<"    5     / 	50              / "<<pago_estacionamiento(5) <<endl;
+	cout <<"    7     / 	63              / "<<pago_estacionamiento(7) <<endl;
+	cout <<"    10    / 	90              / "<<pago_estacionamiento(10) <<endl;
+	cout <<"    11    / 	77              / "<<pago_estacionamiento(11) <<endl;
+			
+	  
 
 
     cout << endl;
